41694021139484bacf95869b0051f7258cce720b
Suman Hessa <sumanhessa006@gmail.com>
2026-04-26 09:48:08 +0530

    chore: keep temp folder for multer

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { config } from './src/config/config.js'
 
 // https://vite.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-      '/api': config.backend_api_url,
+      '/api': "http://localhost:3000",
     }
   },
   plugins: [react(), tailwindcss()],
